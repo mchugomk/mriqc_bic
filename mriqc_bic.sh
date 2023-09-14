bf8e5968b823e0c4535b75114158328280e98cde
Maureen McHugo <maureen.mchugo@cuanschutz.edu>
2023-09-14 10:39:33 -0600

    update version 23.1.0

--- mriqc_bic.sh
+++ mriqc_bic.sh
@@ -4,11 +4,11 @@
 
 
 ## Variables to specify data folders 
-mriqc_version=22.0.6		# mriqc version to run
-nprocs=10 					# run with 10 cores
-mem=10000					# run with 10GB memory
+mriqc_version=23.1.0		# mriqc version to run
+nprocs=10 			# run with 10 cores
+mem=10000			# run with 10GB memory
 work_dir=$HOME/work 		# working directory
-session_id=""				# initialize session id if not specified
+session_id=""			# initialize session id if not specified
 
 
 ## Process command line arguments
